ac40aa0954586eaf084cdedf51dac6005568b7c8
rodrigo <rodrigo@magnoliagrafica.cl>
2024-11-26 10:53:16 -0300

    elimine dashboard btn

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button"
 
 const Dashboard = () => {
     return (
         <div className="ml-28">
-            <h1>dashboard page</h1>
-            <Button>Click me</Button>
+            <h1>dashboard page</h1>       
         </div>
     );
 };

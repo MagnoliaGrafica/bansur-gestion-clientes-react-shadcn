64be70ed50765586cc56074817e49be99f169e18
rodrigo <rodrigo@magnoliagrafica.cl>
2024-12-31 12:54:31 -0300

    nav superior chao

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation";
-import Navsuperior from "../components/Navsuperior";
 
 const DefaultLayout = () => {
   return (
     <div>
       {/* Aquí está el Navigation que será común para todas las rutas */}
       <Navigation />
-    <Navsuperior />
-
+    
       <main className="w-full h-screen flex flex-row relative bg-white pl-28">
         {/* Aquí se renderizarán las rutas secundarias */}
         <Outlet />

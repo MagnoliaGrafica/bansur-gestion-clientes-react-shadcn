b29e0de0a8a5091a6f5beb411ca0cc5acc02af0d
rodrigo <rodrigo@magnoliagrafica.cl>
2025-01-16 14:26:58 -0300

    data table 50%

--- src/components/lista-clientes/columns.tsx
+++ src/components/lista-clientes/columns.tsx
@@ -1,5 +1,5 @@
 import { ColumnDef } from "@tanstack/react-table"
-
+import { Link } from "react-router";
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -11,6 +11,8 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback} from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
+import { PencilSquareIcon, EllipsisHorizontalIcon, ArrowsUpDownIcon } from "@heroicons/react/24/outline";
+
 export type Payment = {
     id: number;
     nombre: string;
@@ -101,204 +103,227 @@ type BadgeVariant =
   const getBadgeVariant = (id: number | undefined): BadgeVariant =>
     badgeVariants[id || 0] || "outline";
 
-export const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: "nombre",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Nombre
-        </Button>
-      );
-    },
-    cell: ({ row }) => {
-      const nombre = row.getValue("nombre") as string;
-      const apellido = row.original.apellido as string;
-      const rut = row.original.rut as string;
-      return (<div className="text-left">{nombre} {apellido} <br />{rut}</div>);
-    },
-  },
-  {
-    accessorKey: "monto",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Monto
-        </Button>
-      );
-    },
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("monto") as string); // Cast explícito
-      const formatted = new Intl.NumberFormat("es-CL", {
-        style: "currency",
-        currency: "CLP",
-      }).format(amount);
-
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
-  },
-  {
-    accessorKey: "montoEvaluar",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Monto a evaluar
-        </Button>
-      );
-    },
-    cell: ({ row }) => {
-      const montoEvaluar = row.getValue("montoEvaluar") as string | number;
+  const capitalizeFirstLetter = (text: string): string => {
+    if (!text) return '';
+    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+  };
   
-      // Verificar si montoEvaluar es un valor numérico válido
-      const amount = isNaN(Number(montoEvaluar)) || montoEvaluar == null || montoEvaluar === "" ? 0 : parseFloat(montoEvaluar.toString());
+
+  export const columns: ColumnDef<Payment>[] = [
+    {
+      accessorKey: "nombre",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Nombre <ArrowsUpDownIcon className="size-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        const nombre = capitalizeFirstLetter(row.getValue("nombre") as string);
+        const apellido = capitalizeFirstLetter(row.original.apellido as string);
+        const rut = row.original.rut as string;
+        const convenio = capitalizeFirstLetter(row.original.gc_convenio?.nombre as string);
+        return (
+          <div className="text-bansur text-base font-medium">
+            {nombre} {apellido} <br />
+            <span className="text-gray-400 text-xs">{rut}</span>
+            <br />
+            <span className="text-gray-800 text-md font-normal">{convenio}</span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "monto",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Monto Solicitado <ArrowsUpDownIcon className="size-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        const amount = parseFloat(row.getValue("monto") as string); // Cast explícito
+        const formatted = new Intl.NumberFormat("es-CL", {
+          style: "currency",
+          currency: "CLP",
+        }).format(amount);
   
-      const formatted = new Intl.NumberFormat("es-CL", {
-        style: "currency",
-        currency: "CLP",
-      }).format(amount);
+        return <div className="text-center">{formatted}</div>;
+      },
+    },
+    {
+      accessorKey: "montoEvaluar",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Monto a evaluar <ArrowsUpDownIcon className="size-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        const montoEvaluar = row.getValue("montoEvaluar") as string | number;
+        const amount =
+          isNaN(Number(montoEvaluar)) || montoEvaluar == null || montoEvaluar === ""
+            ? 0
+            : parseFloat(montoEvaluar.toString());
+        const formatted = new Intl.NumberFormat("es-CL", {
+          style: "currency",
+          currency: "CLP",
+        }).format(amount);
   
-      return <div className="text-right">{formatted}</div>;
+        return <div className="text-center font-medium">{formatted}</div>;
+      },
     },
-  },
+    {
+      accessorKey: "createdAt",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Fecha ingreso <ArrowsUpDownIcon className="size-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        const date = new Date(row.getValue("createdAt") as number);
+        return <div className="text-center">{date.toLocaleDateString("es-CL")}</div>;
+      },
+    },
+    {
+      accessorKey: "fechaCierre",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Fecha cierre <ArrowsUpDownIcon className="size-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        const fechaCierre = row.getValue("fechaCierre") as string | number | null;
+        if (!fechaCierre) {
+          return <div className="text-center">-</div>;
+        }
+        const date = new Date(fechaCierre);
+        return <div className="text-center">{date.toLocaleDateString("es-CL")}</div>;
+      },
+    },
+    {
+      id: "días",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Total días <ArrowsUpDownIcon className="size-4" />
+          </Button>
+        );
+      },
+      accessorFn: (row) => {
+        const createdAt = row.createdAt as string | number;
+        const fechaCierre = row.fechaCierre as string | number | Date | null;
+        return calculateDays(createdAt, fechaCierre);
+      },
+      cell: ({ row }) => {
+        const createdAt = row.getValue("createdAt") as string | number;
+        const fechaCierre = row.getValue("fechaCierre") as string | number | Date | null;
+        const days = calculateDays(createdAt, fechaCierre);
   
-  {
-    accessorKey: "createdAt",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Fecha ingreso
-        </Button>
-      );
+        return <div className="text-center">{days} días</div>;
+      },
     },
-    cell: ({ row }) => {
-      const date = new Date(row.getValue("createdAt") as number);
-      return <div>{date.toLocaleDateString("es-CL")}</div>;
+    {
+      accessorFn: (row) => row.gc_estado?.nombre || "Sin asignar",
+      id: "gc_estado",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Estado <ArrowsUpDownIcon className="size-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        return (
+          <div className="text-center">
+            <Badge variant={getBadgeVariant(row.original.gc_estado?.id)}>
+              {row.original.gc_estado ? row.original.gc_estado?.nombre : "Sin asignar"}
+            </Badge>
+          </div>
+        );
+      },
     },
-  },{
-  accessorKey: "fechaCierre",
-  header: ({ column }) => {
-    return (
-      <Button
-        variant="ghost"
-        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-      >
-        Fecha cierre
-      </Button>
-    );
-  },
-  cell: ({ row }) => {
-    const fechaCierre = row.getValue("fechaCierre") as string | number | null;
-
-    // Verifica si fechaCierre es nula, vacía o indefinida
-    if (!fechaCierre) {
-      return <div>-</div>;
-    }
-
-    const date = new Date(fechaCierre);
-    return <div>{date.toLocaleDateString("es-CL")}</div>;
-  },
-},
-
-  {
-    id: "días",
-    header: ({ column }) => { 
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Total días
-        </Button>
-      );
+    {
+      accessorKey: "ejecutivo",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Ejecutivo <ArrowsUpDownIcon className="size-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        return (
+          <div className="flex justify-center">
+            <Avatar>
+              <AvatarFallback>
+                {getInitials(row.original.gc_ban_user?.nombre, row.original.gc_ban_user.apellido)}
+              </AvatarFallback>
+            </Avatar>
+          </div>
+        );
+      },
     },
-    cell: ({ row }) => {
-      const createdAt = row.getValue("createdAt") as string;
-      const fechaCierre = row.getValue("fechaCierre") as string | number | Date | null;
-      const days = calculateDays(createdAt, fechaCierre);
-
-      return <div>{days} días</div>;
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const payment = row.original;
+        return (
+          <div className="text-center">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="size-8 p-0">
+                  <span className="sr-only">Open menu</span>
+                  <EllipsisHorizontalIcon className="size-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Button asChild variant="outline">
+                    <Link to={`/cliente/${payment.id}`}>
+                      <PencilSquareIcon className="h-4 w-4" />
+                      Editar
+                    </Link>
+                  </Button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        );
+      },
     },
-  },
-
-  {
-    accessorKey: "gc_estado",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Estado
-        </Button>
-      );
-    },
-    cell: ({ row }) => {
-      return (<div>
-      <Badge variant={getBadgeVariant(row.original.gc_estado?.id)}>
-                {row.original.gc_estado ? row.original.gc_estado?.nombre : "Sin asignar"}
-              </Badge>
-              </div>);
-    },
-  },
-
-  {
-    accessorKey:"ejecutivo",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Ejecutivo
-        </Button>
-      );
-    },
-    cell: ({ row }) => {
-      return (<div> <Avatar>
-      <AvatarFallback>
-        {getInitials(row.original.gc_ban_user?.nombre, row.original.gc_ban_user.apellido)}
-      </AvatarFallback>
-    </Avatar></div>);
-    }
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      //const payment = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              ...
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            {/*<DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>*/}
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
+  ];
+  

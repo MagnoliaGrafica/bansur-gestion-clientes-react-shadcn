bb88cf395c997c5082380c75aa7eced1ddf392b7
rodrigo <rodrigo@magnoliagrafica.cl>
2025-03-19 18:48:21 -0300

    informe gg en 90porciento

--- src/pages/infogg.tsx
+++ src/pages/infogg.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 import { getAllClientesgg } from "@/api/clientes";
+import { Button } from "@/components/ui/button"
+import { UserCircleIcon,DocumentArrowDownIcon } from "@heroicons/react/24/outline";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+
 
 // Definir el tipo de estructura de datos
 type TotalesPorSucursal = {
@@ -119,100 +124,209 @@ const Infogg = () => {
     fetchClientes();
   }, []);
 
+  const handleExportPDF = () => {
+    const table = document.getElementById("table-to-export"); // Capturar la tabla
+    if (!table) return;
+
+    html2canvas(table, { scale: 2 }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4");
+      const imgWidth = 190; // Ancho en mm (ajustar según necesidad)
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      pdf.addImage(imgData, "PNG", 10, 10, imgWidth, imgHeight);
+      pdf.save("tabla.pdf");
+    });
+  };
+
   return (
-    <div>
+    <div className="antialiased bg-gray-100 text-gray-600 h-screen w-full px-4 space-y-4 py-3" >
      
-      <h1>Tabla de GG</h1>
-      <p>Esta es la tabla de GG</p>
-
-      <section className="w-full">
-        <h2>Tabla resumen por sucursal</h2>
-      <table className="w-1/2">
-        <thead className="bg-bansur text-white">
-          <tr className="px-5">
-            <th>SUCURSAL</th>
-            <th>PROSPECTO</th>
-            <th>ACEPTADO</th>
-            <th>APROBADO</th>
-            <th>CURSADO</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Osorno</td>
-            <td> {(totales[1]?.["Prospecto"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-            <td> {(totales[1]?.["Aceptado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-            <td> {(totales[1]?.["Aprobado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-            <td> {(totales[1]?.["Cursado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-          </tr>
-          <tr>
-            <td>Puerto Montt</td>
-            <td>{(totales[2]?.["Prospecto"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-            <td> {(totales[2]?.["Aceptado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-            <td> {(totales[2]?.["Aprobado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-            <td> {(totales[2]?.["Cursado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-          </tr>
-          <tr>
-            <td>Temuco</td>
-            <td> {(totales[3]?.["Prospecto"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-            <td> {(totales[3]?.["Aceptado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-            <td> {(totales[3]?.["Aprobado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-            <td> {(totales[3]?.["Cursado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</td>
-          </tr>
-        </tbody>
-        <tfoot className="bg-gray-100">
-        <tr>
-  <td>TOTAL</td>
-  {["Prospecto", "Aceptado", "Aprobado", "Cursado"].map((estado, index) => (
-    <td key={index}>
-      {Object.values(totales)
-        .reduce((acc, sucursal) => acc + (sucursal[estado] || 0), 0)
-        .toLocaleString("es-CL", { style: "currency", currency: "CLP" })}
-    </td>
-  ))}
-</tr>
-        </tfoot>
-      </table>
-      </section>
-
       <section>
-  <h2>Tabla Resumen por Ejecutivos</h2>
-  <table className="w-full">
-    <thead className="bg-bansur text-white">
-      <tr>
-        <th className="w-1/5">EJECUTIVO</th>
-        <th className="w-1/5">PROSPECTO</th>
-        <th className="w-1/5">ACEPTADO</th>
-        <th className="w-1/5">APROBADO</th>
-        <th className="w-1/5">CURSADO</th>
-      </tr>
-    </thead>
-    <tbody>
-      {Object.entries(totalesEjecutivos).map(([ejecutivoNombre, estados]) => (
-        <tr key={ejecutivoNombre}>
-          <td className="p-3">{ejecutivoNombre}</td>
-          {["Prospecto", "Aceptado", "Aprobado", "Cursado"].map((estado, index) => (
-            <td key={index}>
-              {estados[estado].toLocaleString("es-CL", { style: "currency", currency: "CLP" })}
-            </td>
-          ))}
-        </tr>
-      ))}
-    </tbody>
-    <tfoot className="bg-gray-150">
-      <tr>
-        <td>TOTAL</td>
-        {["Prospecto", "Aceptado", "Aprobado", "Cursado"].map((estado, index) => (
-          <td key={index}>
-            {Object.values(totalesEjecutivos)
-              .reduce((acc, ejecutivo) => acc + (ejecutivo[estado] || 0), 0)
-              .toLocaleString("es-CL", { style: "currency", currency: "CLP" })}
-          </td>
-        ))}
-      </tr>
-    </tfoot>
-  </table>
+        <div className="w-full max-w-3xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200 p-4">
+          
+              <Button variant="outline" onClick={handleExportPDF}>
+                <DocumentArrowDownIcon /> Exportar a PDF
+                </Button>
+          
+        </div>
+      </section>
+      <div id="table-to-export" className="space-y-4">
+
+      
+ <section>
+    <div className="flex flex-col justify-center h-full">
+        <div className="w-full max-w-3xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
+            <header className="px-5 py-4 border-b border-gray-100">
+                <h2 className="font-semibold text-gray-800">Resumen por SUCURSAL</h2>
+            </header>
+            <div className="p-3">
+                <div className="overflow-x-auto">
+                    <table className="table-auto w-full">
+                        <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+                            <tr>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-left">Sucursal</div>
+                                </th>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-left">Prospecto</div>
+                                </th>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-left">Aceptado</div>
+                                </th>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-center">Aprobado</div>
+                                </th>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-center">Cursado</div>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-sm divide-y divide-gray-100">
+                            <tr>
+                                <td className="p-2 whitespace-nowrap">
+                                    <div className="flex items-center">
+                                        <div className="font-medium text-gray-800">Osorno</div>
+                                    </div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[1]?.["Prospecto"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[1]?.["Aceptado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[1]?.["Aprobado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[1]?.["Cursado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="p-2 whitespace-nowrap">
+                                    <div className="flex items-center">
+                                        <div className="font-medium text-gray-800">Puerto Montt</div>
+                                    </div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[2]?.["Prospecto"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[2]?.["Aceptado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[2]?.["Aprobado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[2]?.["Cursado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="p-2 whitespace-nowrap">
+                                    <div className="flex items-center">
+                                        <div className="font-medium text-gray-800">Temuco</div>
+                                    </div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[3]?.["Prospecto"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[3]?.["Aceptado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[3]?.["Aprobado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                <td className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{(totales[3]?.["Cursado"] || 0).toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                            </tr>
+                                       
+                        </tbody>
+                        <tfoot className="text-md font-semibold uppercase text-bansur bg-gray-50 p-10">
+                        <tr>
+                          <td><span className="ml-2">TOTAL</span></td>
+                          {["Prospecto", "Aceptado", "Aprobado", "Cursado"].map((estado, index) => (
+                            <td key={index}>
+                              {Object.values(totales)
+                                .reduce((acc, sucursal) => acc + (sucursal[estado] || 0), 0)
+                                .toLocaleString("es-CL", { style: "currency", currency: "CLP" })}
+                            </td>
+                          ))}
+                        </tr>
+                      </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
+
+
+<section>
+    <div className="flex flex-col justify-center h-full">
+        <div className="w-full max-w-3xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
+            <header className="px-5 py-4 border-b border-gray-100">
+                <h2 className="font-semibold text-gray-800">Resumen por EJECUTIVO</h2>
+            </header>
+            <div className="p-3">
+                <div className="overflow-x-auto">
+                    <table className="table-auto w-full">
+                        <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+                            <tr>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-left">Ejecutivo</div>
+                                </th>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-left">Prospecto</div>
+                                </th>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-left">Aceptado</div>
+                                </th>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-center">Aprobado</div>
+                                </th>
+                                <th className="p-2 whitespace-nowrap">
+                                    <div className="font-semibold text-center">Cursado</div>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-sm divide-y divide-gray-100">
+                        {Object.entries(totalesEjecutivos).map(([ejecutivoNombre, estados]) => (
+                            <tr key={ejecutivoNombre}>
+                                <td className="p-2 whitespace-nowrap">
+                                    <div className="flex items-center">
+                                        <div className="flex-shrink-0 py-2 mr-2 sm:mr-3"><UserCircleIcon className="size-7 text-bansur" /></div>
+                                        <div className="font-medium text-gray-800">{ejecutivoNombre}</div>
+                                    </div>
+                                </td>
+                                {["Prospecto", "Aceptado", "Aprobado", "Cursado"].map((estado, index) => (
+                                <td key={index} className="p-2 whitespace-nowrap">
+                                  <div className="text-left text-bansur">{estados[estado].toLocaleString("es-CL", { style: "currency", currency: "CLP" })}</div>
+                                </td>
+                                ))}
+                            </tr>
+                         ))}               
+                        </tbody>
+                        <tfoot className="text-md font-semibold uppercase text-bansur bg-gray-50 p-3">
+                          <tr>
+                            <td><span className="pl-2">TOTAL</span></td>
+                            {["Prospecto", "Aceptado", "Aprobado", "Cursado"].map((estado, index) => (
+                              <td key={index}>
+                                {Object.values(totalesEjecutivos)
+                                  .reduce((acc, ejecutivo) => acc + (ejecutivo[estado] || 0), 0)
+                                  .toLocaleString("es-CL", { style: "currency", currency: "CLP" })}
+                              </td>
+                            ))}
+                          </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+
     </div>
   );
 };
